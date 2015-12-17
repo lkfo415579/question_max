662950d2d3e8a68cbd20466ba930be429aaf0364
lkfo415579 <gm0648@hotmail.com>
2015-12-17 23:03:46 +0800

    hey

--- question.py
+++ question.py
@@ -1,4 +1,17 @@
-
+def count_match_dict(target, list):
+	try:
+		#candidate_list = my_dict[source]
+		#print list
+		#print 'list word : %s ' % target
+		for list_word in list:
+			if target == list_word:
+				return 1
+		return 0
+			# print '-target- ' + target +' -word- ' + word + ' -source- ' + source
+		#
+	except KeyError:
+		return 0
+		
 def get_featureset():
 	import xml.etree.cElementTree as ET
 	tree = ET.ElementTree(file='data/train.xml')
@@ -17,25 +30,46 @@ def get_featureset():
 		features['QCATEGORY'] = data.attrib['QCATEGORY']
 		features['QGOLD_YN'] = data.attrib['QGOLD_YN']
 		#features['comments'] = []
-		QSubject = data[0]
-		features['QSubject'] = tuple(QSubject.text.split())
-		QBody = data[1]
-		features['QBody'] = tuple(QBody.text.split())
+		QSubject = data[0].text.split()
+		#features['QSubject'] = tuple(QSubject.text.split())
+		QBody = data[1].text.split()
+		#features['QBody'] = tuple(QBody.text.split())
 		
 		del data[0]
 		del data[0]
 		for node in data:
 			#print node
 			tag = node.attrib['CGOLD']
-			features['CSubject'] = tuple(node[0].text.split())
-			features['CBody'] = tuple(node[1].text.split())
+			CSubject = node[0].text.split()
+			CBody =  node[1].text.split()
+			#features['CSubject'] = tuple(node[0].text.split())
+			#features['CBody'] = tuple(node[1].text.split())
+			
+			#count match words
+			CSword_num = 0
+			CBword_num = 0
+			for CS_word in CSubject:
+				if count_match_dict(CS_word,QSubject) == 1:
+					CSword_num = CSword_num + 1
+				features['CSword(%s)' % CS_word] = 1
+				
+				
+			for CB_word in CBody:
+				if count_match_dict(CB_word,QBody) == 1:
+					CBword_num = CBword_num + 1
+				features['CBword(%s)' % CB_word] = 1
+				
+			features['CSword_num'] = CSword_num
+			features['CBword_num'] = CBword_num
+			
 			featureset.append([features,tag])
 		return featureset
 		#features['question'] = 
 	for node_question in root:
 		featureset = featureset + gender_features(node_question)
 		
-	#print featureset[0]
-	#print featureset[1]
-	#print featureset[2]
+	#print featureset[0:20]
+
+	
+	
 	return featureset
